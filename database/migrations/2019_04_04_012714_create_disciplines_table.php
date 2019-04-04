158d32f83129ff92e8bce1ec99aa700848fa8d97
Alabee <ianmutevu96@gmail.com>
2019-04-03 18:44:24 -0700

    Discipline table added

---
+++ database/migrations/2019_04_04_012714_create_disciplines_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDisciplinesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('disciplines', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+            $table->string('reg_no');
+            $table->string('student_name');
+            $table->string('status');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('disciplines');
+    }
+}
