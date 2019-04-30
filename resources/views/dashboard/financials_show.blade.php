48eea438dcb36ba6cb9fb5d684936ec7e1178c8d
Alabee <ianmutevu96@gmail.com>
2019-04-30 08:14:26 -0700

    Minor fixes

--- resources/views/dashboard/financials_show.blade.php
+++ resources/views/dashboard/financials_show.blade.php
@@ -1,24 +1,10 @@
 @extends('layouts.admin')
 @section('content')
 	<h1>Financial Information</h1>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a href="/financials/add"><button class="btn btn-outline-success my-2 my-sm-0" type="submit">Add student financials</button></a>
-    </form>
-      </li>
-    </ul>
-    <form class="form-inline my-2 my-lg-0">
+	    <form class="form-inline my-2 my-lg-0">
       <input class="form-control mr-sm-2" type="search" placeholder="Search for student" aria-label="Search">
       <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
     </form>
-  </div>
-</nav>
 
 	<table class="table table-striped">
 	    <tr>
