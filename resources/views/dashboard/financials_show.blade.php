2dad354598324fc1c162f37b5c0bfdd1aea8889c
Alabee <ianmutevu96@gmail.com>
2019-05-02 18:10:06 -0700

    Minor fix search box

--- resources/views/dashboard/financials_show.blade.php
+++ resources/views/dashboard/financials_show.blade.php
@@ -1,8 +1,9 @@
 @extends('layouts.admin')
 @section('content')
+	
+	<form class="form-inline my-2 my-lg-0">
 	<h1>Financial Information</h1>
-	    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search for student" aria-label="Search">
+      <input class="form-control mr-sm-2 ml-auto" type="search" placeholder="Search for student" aria-label="Search">
       <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
     </form>
 
